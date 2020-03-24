efc94e1440d664cd6cd67263f8c5fbaad3e841dd
Maxxie <max.umberto97@gmail.com>
2020-03-24 19:58:59 -0300

    back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+// Importa as funcionalidades do express
+const express = require ('express');
+
+const cors = require('cors');
+
+//Pega as rotas importas
+const routes = require('./routes');
+
+//Criando a aplicaçãp
+const app = express();
+
+app.use(cors);
+
+//Informa que usaremos JSON para as requisições
+app.use(express.json());
+
+app.use(routes)
+
+//Ouvir a porta 3333 (localhost:3333)
+app.listen(3333);
---
+++ backend/src/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes =  express.Router();
+
+//Rota
+
+routes.post('/sessions', SessionController.create)
+
+routes.post('/ongs', OngController.create);
+routes.get('/ongs', OngController.index);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+//Exportar as rotas para ficarem visíveis na index.js
+module.exports = routes;
